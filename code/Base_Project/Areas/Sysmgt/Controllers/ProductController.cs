53a2efa9a99bb9caf62e5297fc8ec433dbb61dff
jimmy <jimmy@iifun.com.tw>
2019-06-03 13:26:15 +0800

    商品基本cru

--- code/Base_Project/Areas/Sysmgt/Controllers/ProductController.cs
+++ code/Base_Project/Areas/Sysmgt/Controllers/ProductController.cs
@@ -8,122 +8,232 @@ using System.Web;
 using System.Web.Mvc;
 using Base_Project.Entity;
 using EntityState = System.Data.Entity.EntityState;
-using Base_Project.Models;
+using Base_Project.Models.Product;
+using Base_Project.Helpers;
 
 namespace Base_Project.Areas.Sysmgt.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : BaseController
     {
-        private DBEntities db = new DBEntities();
-
-        // GET: PRODUCT
+        #region Index
         public ActionResult Index()
         {
-            return View(db.PRODUCTs.ToList());
-        }
+            var model = new IndexViewModel();
 
-        // GET: PRODUCT/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PRODUCT pRODUCT = db.PRODUCTs.Find(id);
-            if (pRODUCT == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pRODUCT);
-        }
+            GetPageList(model);
 
-        // GET: PRODUCT/Create
-        public ActionResult Create()
-        {
-            return View();
+            return View(model);
         }
-
-        // POST: PRODUCT/Create
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
-        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "SID,CSID,MSID,CDT,MDT,ENABLED,NAME,PRICE,IMG_SRC,DESC,CONTENT")] PRODUCT pRODUCT)
+        public ActionResult GetList(IndexViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                db.PRODUCTs.Add(pRODUCT);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(pRODUCT);
+            GetPageList(model);
+            return View("_PageList", model);
         }
+        private void GetPageList(IndexViewModel model)
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                try
+                {
+                    var products = db.PRODUCTs.Select(x => x);
+                    if (!String.IsNullOrEmpty(model.Keyword))
+                    {
+                        products = products.Where(x => x.NAME.Contains(model.Keyword));
+                    }
 
-        // GET: PRODUCT/Edit/5
+                    model.TotalCount = products.Count();
+                    model.PageSize = 10;
+
+                    var totalPage = (model.TotalCount + model.PageSize - 1) / model.PageSize;
+                    var startRow = model.PageSize * (model.PageIndex - 1);
+                    var num = startRow + 1;
+
+                    products = products.OrderByDescending(x => x.CDT).Skip(startRow).Take(model.PageSize);
+
+                    foreach (var item in products.ToList())
+                    {
+                        model.Items.Add(new PageListItem()
+                        {
+                            Num = num++.ToString(),
+                            SId = item.SID,
+                            Mdt = item.MDT.ToString("yyyy年MM月dd日 HH:mm:ss"),
+                            Name = item.NAME,
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                    throw ex;
+                }
+            }
+        }
+        #endregion
+
+        #region Edit
         public ActionResult Edit(string id)
         {
-            if (id == null)
+            var model = new EditViewModel();
+            if (String.IsNullOrEmpty(id))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return View(model);
             }
-            PRODUCT pRODUCT = db.PRODUCTs.Find(id);
-            if (pRODUCT == null)
+            else
             {
-                return HttpNotFound();
+                using (DBEntities db = new DBEntities())
+                {
+                    try
+                    {
+                        PRODUCT product = db.PRODUCTs.Find(id);
+                        if (product == null)
+                        {
+                            return HttpNotFound();
+                        }
+
+                        //帶入資料
+                        model.CDT = product.CDT;
+                        model.CSID = product.CSID;
+                        model.ENABLED = product.ENABLED;
+                        model.MDT = product.MDT;
+                        model.MSID = product.MSID;
+                        model.NAME = product.NAME;
+                        model.SID = product.SID;
+                        model.CAT_SID = product.CAT_SID;
+                        model.CONTENT = product.CONTENT;
+                        model.DESC = product.DESC;
+                        model.IMG_SRC = product.IMG_SRC;
+                        model.PRICE = product.PRICE;
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Error(ex.Message);
+                        throw ex;
+                    }
+                }
+
+                return View(model);
             }
-            return View(pRODUCT);
         }
 
-        // POST: PRODUCT/Edit/5
-        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
-        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "SID,CSID,MSID,CDT,MDT,ENABLED,NAME,PRICE,IMG_SRC,DESC,CONTENT")] PRODUCT pRODUCT)
+        public JsonResult Edit(EditViewModel model)
         {
-            if (ModelState.IsValid)
+            List<ErrorMsg> ErrorMsgs = new List<ErrorMsg>();
+            //新增
+            if (String.IsNullOrEmpty(model.SID))
             {
-                db.Entry(pRODUCT).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(pRODUCT);
-        }
 
-        // GET: PRODUCT/Delete/5
-        public ActionResult Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            PRODUCT pRODUCT = db.PRODUCTs.Find(id);
-            if (pRODUCT == null)
-            {
-                return HttpNotFound();
-            }
-            return View(pRODUCT);
-        }
+                model.SID = SystemIdHelper.getNewSId();
 
-        // POST: PRODUCT/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
-        {
-            PRODUCT pRODUCT = db.PRODUCTs.Find(id);
-            db.PRODUCTs.Remove(pRODUCT);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+                PRODUCT product = new PRODUCT()
+                {
+                    SID = model.SID,
+                    CDT = DateTime.Now,
+                    MDT = DateTime.Now,
+                    CSID = SystemIdHelper.getEmptySId(),
+                    MSID = SystemIdHelper.getEmptySId(),
+                    ENABLED = model.ENABLED,
+                    NAME = model.NAME,
+                    CAT_SID = model.CAT_SID,
+                    CONTENT = model.CONTENT,
+                    DESC = model.DESC,
+                    IMG_SRC = model.IMG_SRC,
+                    PRICE = model.PRICE,
+                };
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
+                if (ErrorMsgs.Count > 0)
+                {
+                    return Json(new { Success = false, ErrorMsgs });
+                }
+                using (DBEntities db = new DBEntities())
+                {
+                    try
+                    {
+                        db.PRODUCTs.Add(product);
+                        db.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorMsgs.Add(new ErrorMsg() { ErrorID = "Utility", ErrorText = ex.Message });
+                        return Json(new { Success = false, ErrorMsgs });
+                    }
+                }
             }
-            base.Dispose(disposing);
+            //修改
+            else
+            {
+                //查詢既有資料
+                using (DBEntities db = new DBEntities())
+                {
+                    var oldInfo = db.PRODUCTs.AsNoTracking().Where(x => x.SID == model.SID).Single();
+                    PRODUCT newInfo = new PRODUCT()
+                    {
+                        SID = model.SID,
+                        CDT = oldInfo.CDT,
+                        MDT = DateTime.Now,
+                        CSID = oldInfo.CSID,
+                        MSID = SystemIdHelper.getEmptySId(),
+                        ENABLED = model.ENABLED,
+                        NAME = model.NAME,
+                        CAT_SID = model.CAT_SID,
+                        CONTENT = model.CONTENT,
+                        DESC = model.DESC,
+                        IMG_SRC = model.IMG_SRC,
+                        PRICE = model.PRICE,
+                    };
+
+                    if (ErrorMsgs.Count > 0)
+                    {
+                        return Json(new { Success = false, ErrorMsgs });
+                    }
+                    db.Entry(newInfo).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+            }
+            return Json(new { Success = true });
         }
+        #endregion
+
+        #region Delete
+        public JsonResult Delete(FormCollection form)
+        {
+            List<ErrorMsg> ErrorMsgs = new List<ErrorMsg>();
+
+            if (String.IsNullOrEmpty(form["selectItems"]))
+            {
+                ErrorMsgs.Add(new ErrorMsg() { ErrorID = "Utility", ErrorText = "請選擇至少一個項目" });
+                return Json(new { Success = false, ErrorMsgs });
+            }
+
+            foreach (var sid in form["selectItems"].Split(','))
+            {
+                if (!SystemIdHelper.checkSId(sid))
+                {
+                    ErrorMsgs.Add(new ErrorMsg() { ErrorID = "Utility", ErrorText = "包含不合法的系統代號" });
+                    return Json(new { Success = false, ErrorMsgs });
+                }
+            }
+            foreach (var sid in form["selectItems"].Split(','))
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    try
+                    {
+                        var product = db.PRODUCTs.Single(x => x.SID == sid);
+                        db.PRODUCTs.Remove(product);
+                        db.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        ErrorMsgs.Add(new ErrorMsg() { ErrorID = "Utility", ErrorText = ex.Message });
+                        return Json(new { Success = false, ErrorMsgs });
+                    }
+                }
+            }
+            return Json(new { Success = true });
+        }
+        #endregion
     }
 }
---
+++ code/Base_Project/Areas/Sysmgt/Models/ProductViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Base_Project.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Base_Project.Models.Product
+{
+    #region IndexViewModel
+    public class IndexViewModel : PageListViewModel
+    {
+        public List<PageListItem> Items { get; set; } = new List<PageListItem>();
+    }
+    public class PageListItem : IPageListItem
+    {
+        /// <summary>
+        /// 項次
+        /// </summary>
+        public string Num { get; set; }
+        /// <summary>
+        /// 系統代號
+        /// </summary>
+        public string SId { get; set; }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public string Mdt { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+    }
+    #endregion
+
+    #region EditViewModel
+    public class EditViewModel : BaseViewModel {
+    #region 表單欄位
+    [Display(Name = "系統代號")]
+    public string SID { get; set; }
+    [Display(Name = "創建人")]
+    public string CSID { get; set; }
+    [Display(Name = "修改人")]
+    public string MSID { get; set; }
+    [Display(Name = "創建日期"), Required]
+    public System.DateTime CDT { get; set; }
+    [Display(Name = "修改日期"), Required]
+    public System.DateTime MDT { get; set; }
+    [Display(Name = "啟用"), Required]
+    public virtual bool ENABLED { get; set; }
+    [Display(Name = "姓名"), Required, MaxLength(100)]
+    public string NAME { get; set; }
+    [Display(Name = "分類")]
+    public string CAT_SID { get; set; }
+    [Display(Name = "價格") , Range(0,int.MaxValue)]
+    public Nullable<int> PRICE { get; set; }
+    [Display(Name = "列表頁圖片")]
+    public string IMG_SRC { get; set; }
+    [Display(Name = "簡述")]
+    public string DESC { get; set; }
+    [Display(Name = "內文")]
+    public string CONTENT { get; set; }
+        #endregion
+    }
+    #endregion
+}
